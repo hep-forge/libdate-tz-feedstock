79416e76d8ad04df9f5f68d4e961126f773c7b4d
Marco Meyer-Conde <marco@tcu.ac.jp>
2025-04-12 20:10:03 +0900

    update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,4 +5,5 @@ cd build
 
 # date_test_pass_parse_bin: $SRC_DIR/test/date_test/parse.pass.cpp:203: void test_c(): Assertion `!in.fail()' failed.
 cmake -DENABLE_DATE_TESTING=OFF -DBUILD_TZ_LIB=ON ../
-cmake --build . --target testit -j$(nproc)
+make -j$(nproc)
+make install
