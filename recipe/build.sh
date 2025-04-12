52ee85d0e5ba124e50701f5ae71a1e0688106a2f
Marco Meyer-Conde <marco@tcu.ac.jp>
2025-04-12 20:13:35 +0900

    update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,6 @@ mkdir build
 cd build
 
 # date_test_pass_parse_bin: $SRC_DIR/test/date_test/parse.pass.cpp:203: void test_c(): Assertion `!in.fail()' failed.
-cmake -DENABLE_DATE_TESTING=OFF -DBUILD_TZ_LIB=ON ../
+cmake -DENABLE_DATE_TESTING=OFF -DBUILD_TZ_LIB=ON -DCMAKE_INSTALL_PREFIX=$PREFIX ..
 make -j$(nproc)
 make install
